0a1c142d507b8fb48d0ad40f531ba1f0b9f52fa4
kvijaya-datascientist <61687772+kvijaya-datascientist@users.noreply.github.com>
2020-03-02 16:41:38 +0530

    Add files via upload
    Updating json documents in Python

---
+++ UpdateQueryTest.py
@@ -0,0 +1,71 @@
+# Updating JSON Documents using Python
+
+import pymongo
+
+client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
+mydb = client['Journal']
+Inventory = mydb.Inventory
+"""Inventory.insert_many([
+    {"item": "canvas",
+     "qty": 100,
+     "size": {"h": 28, "w": 35.5, "uom": "cm"},
+     "status": "A"},
+    {"item": "journal",
+     "qty": 25,
+     "size": {"h": 14, "w": 21, "uom": "cm"},
+     "status": "A"},
+    {"item": "mat",
+     "qty": 85,
+     "size": {"h": 27.9, "w": 35.5, "uom": "cm"},
+     "status": "A"},
+    {"item": "mousepad",
+     "qty": 25,
+     "size": {"h": 19, "w": 22.85, "uom": "cm"},
+     "status": "P"},
+    {"item": "notebook",
+     "qty": 50,
+     "size": {"h": 8.5, "w": 11, "uom": "in"},
+     "status": "P"},
+    {"item": "paper",
+     "qty": 100,
+     "size": {"h": 8.5, "w": 11, "uom": "in"},
+     "status": "D"},
+    {"item": "planner",
+     "qty": 75,
+     "size": {"h": 22.85, "w": 30, "uom": "cm"},
+     "status": "D"},
+    {"item": "postcard",
+     "qty": 45,
+     "size": {"h": 10, "w": 15.25, "uom": "cm"},
+     "status": "A"},
+    {"item": "sketchbook",
+     "qty": 80,
+     "size": {"h": 14, "w": 21, "uom": "cm"},
+     "status": "A"},
+    {"item": "sketch pad",
+     "qty": 95,
+     "size": {"h": 22.85, "w": 30.5, "uom": "cm"},
+     "status": "A"}])   """
+
+#Updating one document
+"""Inventory.update_one(
+    {'item':'sketch pad'},
+    {'$set':{'size.h':22.0,'status':'B'}}
+ )  """
+
+#Updating many documents
+Inventory.update_many(
+    {'qty':{'$lt':40}},
+    {'$set':{'size.uom':'kvl','status':'A'}}
+)
+
+#replace existing doc
+Inventory.replace_one(
+    {'item':'paper'},
+    {'item':'paper',
+     'instock':[
+         {'warehouse':'A','qty':30},
+         {'warehouse':'B','qty':45}
+            ]
+     }
+)
